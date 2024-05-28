9039dd7f6e2c8383d06b451d9b53d0ddb296b778
VipinS47 <165105077+VipinS47@users.noreply.github.com>
2024-05-28 18:18:58 -0400

    Initial commit

---
+++ examples/c/psdoom/psdoom-src/xdoomsrc/contrib/glxdoom.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+#
+# If you modified LDIR in the top level makefile, you need to modify
+# it here to the same directory too.
+#
+
+# *** PID BEGIN ***
+# Changed this to use a psdoom directory rather than xdoom.
+LDIR=/usr/local/games/psdoom
+# old code:
+#LDIR=/usr/local/games/xdoom
+# *** PID END ***
+
+[ "$DOOMWADDIR" ] || DOOMWADDIR=${LDIR}
+PATH=${LDIR}:${PATH}
+export DOOMWADDIR PATH
+
+# *** PID BEGIN ***
+cd ${LDIR}
+exec ./ps-glxdoom $*
+# old code:
+#exec ${LDIR}/glxdoom $*
+# *** PID END ***
+
